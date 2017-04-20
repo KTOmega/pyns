cf8675c42842b9c5091397a01afd4ca49aca4a15
Kevin Tran <me@kevintran.codes>
2017-04-20 16:07:56 -0700

    an example config if you wanted to use this for some reason

---
+++ config.example.py
@@ -0,0 +1,9 @@
+key_file="serv-wksp.private"
+cache_dir="cache"
+nameserver="ns.ktomega.net"
+zone="serv-wksp.lolhax.io"
+db="names.json"
+code="SOME SUPER SECRET CODE NO ONE CAN EVER GUESS"
+secret_key="a randomly generated key"
+
+slack_webhook="a slack api incoming webhook"
